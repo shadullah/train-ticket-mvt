4c8abd12d039b5547dc521ca23c15704978a6ff0
hexaalif <shadullahsad9@gmail.com>
2024-03-16 15:43:56 +0600

    dashboard info doen

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from .views import dashboard
+from .views import dashboard, users_all, borrowed_list
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
+    path('users/', users_all, name='users'),
+    path('borrowed/', borrowed_list, name='borrowed'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from train.models import BorrowedTicket
+from account.models import UserAccount
 
 # Create your views here.
 def dashboard(req):
@@ -7,3 +8,11 @@ def dashboard(req):
     return render(req, 'dashboard/dashboard.html',{
         'ticket': bought_ticket
     })
+
+def users_all(req):
+    users = UserAccount.objects.all()
+    return render(req, 'dashboard/AllUsers.html',{'users': users})
+
+def borrowed_list(req):
+    borrowed = BorrowedTicket.objects.all()
+    return render(req, 'dashboard/borrow_tickets.html', {'borroweds': borrowed})
